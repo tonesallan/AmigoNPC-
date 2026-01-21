493d12fbb863bab871af6ea228b6338cdd081700
Tones Allan <46937531+tonesallan@users.noreply.github.com>
2026-01-20 22:50:29 -0400

    Fix plugin init signature and remove hard API imports

--- src/main/java/br/tones/amigonpc/AmigoNPCPlugin.java
+++ src/main/java/br/tones/amigonpc/AmigoNPCPlugin.java
@@ -1,23 +1,31 @@
 package br.tones.amigonpc;
 
 import br.tones.amigonpc.commands.AmigoCommand;
-import br.tones.amigonpc.commands.AmigoUi2Command;
 import br.tones.amigonpc.commands.AmigoDebugCommand;
+import br.tones.amigonpc.commands.AmigoUi2Command;
+import br.tones.amigonpc.commands.AmigoUiCommand;
 import br.tones.amigonpc.core.AmigoService;
 
 import com.hypixel.hytale.server.core.command.system.CommandManager;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 
 public final class AmigoNPCPlugin extends JavaPlugin {
 
     private AmigoService service;
 
-    @Override
+    public AmigoNPCPlugin(JavaPluginInit init) {
+        super(init);
+    }
+
     public void onEnable() {
         this.service = new AmigoService();
 
         // Comando principal: /amigo
         CommandManager.get().registerSystemCommand(new AmigoCommand(service));
+
+        // UI v1: /amigoui
+        CommandManager.get().registerSystemCommand(new AmigoUiCommand());
 
         // UI v2: /amigoui2
         CommandManager.get().registerSystemCommand(new AmigoUi2Command());
@@ -27,6 +35,5 @@ public final class AmigoNPCPlugin extends JavaPlugin {
                 AmigoDebugCommand.createAsStandalone()
         );
 
-        getLogger().info("[AmigoNPC] Carregado!");
     }
 }
